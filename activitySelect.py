d44fde46fb6e42130c9cd57543cb970b5b285269
Christian D Tuen <christiandt@gmail.com>
2016-01-03 19:38:46 +0100

    integrated controller into UI

--- activitySelect.py
+++ activitySelect.py
@@ -1,15 +1,17 @@
 from sys import exit
+from controller import Controller
 from PyQt4 import QtCore, QtGui
 
 class ActivitySelect(QtGui.QDialog):
 
-    def __init__(self):
+    def __init__(self, controller):
         super(ActivitySelect, self).__init__()
+
+        self.controller = controller
 
         self.activities = dict()
         self.setActivities()
         
-        self.chats = dict()
         title = "Desktop Harmony"
         self.setWindowTitle(str(title))
         self.resize(300, 380)
@@ -20,13 +22,14 @@ class ActivitySelect(QtGui.QDialog):
         self.list = QtGui.QListWidget()
         activityList = list(self.activities)
         self.list.addItems(activityList)
-        self.list.setStyleSheet("font: 25pt")
+        self.list.setStyleSheet("font: 18pt")
         self.list.setSpacing(3)
         self.layout.addWidget(self.list, 1, 0, 1, 2)
 
         #selectbutton
         self.selectbutton = QtGui.QPushButton(self)
         self.selectbutton.setText("Start")
+        self.selectbutton.setStyleSheet("font: 15pt")
         self.selectbutton.setMinimumWidth(20)
         self.selectbutton.setMinimumHeight(50)
         self.layout.addWidget(self.selectbutton, 2, 0)
@@ -35,6 +38,7 @@ class ActivitySelect(QtGui.QDialog):
         #logoutbutton
         self.logoutbutton = QtGui.QPushButton(self)
         self.logoutbutton.setText("Log out")
+        self.logoutbutton.setStyleSheet("font: 15pt")
         self.logoutbutton.setMinimumWidth(5)
         self.logoutbutton.setMinimumHeight(50)
         self.layout.addWidget(self.logoutbutton, 2, 1)
@@ -46,11 +50,13 @@ class ActivitySelect(QtGui.QDialog):
 
     def setActivities(self):
         global activities
-        self.activities = {"Watch TV":12345, "Play Games":12346, "Watch Movies":12347, "Watch AppleTV":12348, "Watch FireTV":12349, "Watch WDTV":12350, "SONOS":12351, "Power OFF":12352}
+        for activity in self.controller.get_activities():
+            self.activities[activity['label']] = activity['id']
 
     def activityStarted(self):
-        activityID = self.activities[str(self.list.currentItem().text())]
-        print activityID
+        activityID = int(self.activities[str(self.list.currentItem().text())])
+        self.controller.start_activity(activityID)
 
     def logOut(self):
+        self.controller.disconnect()
         exit(0)
--- userPref.py
+++ userPref.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore, QtGui
 from error import Error
 from activitySelect import ActivitySelect
+from controller import Controller
 
 class UserPref(QtGui.QDialog):
 
@@ -14,7 +15,7 @@ class UserPref(QtGui.QDialog):
 
         #username input
         self.userNameInput = QtGui.QLineEdit('', self)
-        self.userNameInput.setStyleSheet("font: 20pt")
+        self.userNameInput.setStyleSheet("font: 15pt")
         self.userNameInput.setFixedHeight(30)
         self.layout.addWidget(self.userNameInput, 0, 1, 1, 2)
         userLabel = QtGui.QLabel('Username:')
@@ -23,25 +24,41 @@ class UserPref(QtGui.QDialog):
         #password input
         self.passwordInput = QtGui.QLineEdit('', self)
         self.passwordInput.setEchoMode(QtGui.QLineEdit.Password)
-        self.passwordInput.setStyleSheet("font: 20pt")
+        self.passwordInput.setStyleSheet("font: 15pt")
         self.passwordInput.setFixedHeight(30)
         self.layout.addWidget(self.passwordInput, 1, 1, 1, 2)
         userLabel = QtGui.QLabel('Password:')
         self.layout.addWidget(userLabel, 1, 0)
 
+        #ip input
+        self.ipInput = QtGui.QLineEdit('', self)
+        self.ipInput.setStyleSheet("font: 15pt")
+        self.ipInput.setFixedHeight(30)
+        self.layout.addWidget(self.ipInput, 2, 1, 1, 2)
+        userLabel = QtGui.QLabel('IP:')
+        self.layout.addWidget(userLabel, 2, 0)
+
+        #port input
+        self.portInput = QtGui.QLineEdit('5222', self)
+        self.portInput.setStyleSheet("font: 15pt")
+        self.portInput.setFixedHeight(30)
+        self.layout.addWidget(self.portInput, 3, 1, 1, 2)
+        userLabel = QtGui.QLabel('Port:')
+        self.layout.addWidget(userLabel, 3, 0)
+
         #Remember checkbox
         self.rememberBox = QtGui.QCheckBox()
         self.rememberBox.setChecked(True)
-        self.layout.addWidget(self.rememberBox, 2, 1, 1, 1)
+        self.layout.addWidget(self.rememberBox, 4, 1, 1, 1)
         rememberLabel = QtGui.QLabel('Remember:')
-        self.layout.addWidget(rememberLabel, 2, 0)
+        self.layout.addWidget(rememberLabel, 4, 0)
 
         #OK buton
         self.okbutton = QtGui.QPushButton(self)
         self.okbutton.setText("OK")
         self.okbutton.setMinimumWidth(50)
         self.okbutton.setMinimumHeight(45)
-        self.layout.addWidget(self.okbutton, 3, 1, 1, 1)
+        self.layout.addWidget(self.okbutton, 5, 1, 1, 1)
         QtCore.QObject.connect(self.okbutton, QtCore.SIGNAL("clicked()"), self.signIn)
 
         #Cancel buton
@@ -49,7 +66,7 @@ class UserPref(QtGui.QDialog):
         self.cancelButton.setText("Cancel")
         self.cancelButton.setMinimumWidth(50)
         self.cancelButton.setMinimumHeight(45)
-        self.layout.addWidget(self.cancelButton, 3, 2, 1, 1)
+        self.layout.addWidget(self.cancelButton, 5, 2, 1, 1)
         QtCore.QObject.connect(self.cancelButton, QtCore.SIGNAL("clicked()"), self.cancel)
 
         self.setLayout(self.layout)
@@ -58,11 +75,16 @@ class UserPref(QtGui.QDialog):
     def signIn(self):
         username = str(self.userNameInput.text())
         password = str(self.passwordInput.text())
-        if True:
-            self.activityWindow = ActivitySelect()
+        ip = str(self.ipInput.text())
+        port = str(self.portInput.text())
+
+        try:
+            self.controller = Controller(username, password, ip, port)
+            self.controller.login()
+            self.activityWindow = ActivitySelect(self.controller)
             self.accept()
-        else:
-            self.error = Error("Invalid UN or PW")
+        except Exception, e:
+            self.error = Error(str(e))
 
     def cancel(self):
         self.accept()
